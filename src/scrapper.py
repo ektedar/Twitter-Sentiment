63813de0fc4a53eea9cb01b139cdd91515459caa
Ektedar <ektedar_alam@live.com>
2018-12-08 20:18:56 -0500

    Added Selenium need work on Vader and test API

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,15 +1,13 @@
 '''
 This Python code will scrape the Twitter Account given to it.  This will include information such as User bio and Tweets
-@author: Ektedar Alam
 '''
-
+import time
 import urllib
 import urllib.request
+import argparse
 import bs4
+from selenium import webdriver
 from textblob import TextBlob as tb
-
-USER_URL = "<USER URL>"
-USER_PAGE = urllib.request.urlopen(USER_URL)
 
 # Variable Initializations for readability
 def get_user_info(page):
@@ -18,6 +16,24 @@ def get_user_info(page):
     bio = soup.find('div', {'class':'ProfileHeaderCard'}).find('p').text
     location = str(soup.find('div', {'class':'ProfileHeaderCard-location'}).text).replace("\n", "").replace(" ", "")
     return handle, bio, location
+
+# Generate an infinite scroll of the provided page
+def infinite_scroll(page):
+    print(page)
+    driver = webdriver.Chrome()
+    print("Starting")
+    driver.get(page)
+    scroll_page_time = 1.5
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while(True):
+        print(last_height)
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        time.sleep(scroll_page_time)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        print(new_height, last_height)
+        if new_height == last_height:
+            break
+        last_height = new_height
 
 # Calcualte sentiment of whatever string is being passed
 def textblob_sentiment(tweet):
@@ -30,9 +46,20 @@ def get_tweets(page):
     soup = bs4.BeautifulSoup(page, 'html.parser')
     for content in soup.findAll('div', {'class':'content'}):
         tweet = content.find('p').text
-        sentiment = textblob_sentiment(tweet)
-        print("TWEET: " + str(tweet) + " SENTIMENT: "+ str(sentiment))
-       
+        # sentiment = textblob_sentiment(tweet)
+        print(tweet)
+        # print(sentiment)
+
+# Get the twitter page of the user as added through the CMD line
+def main(user):
+    user_url = f"https://www.twitter.com/{user}"
+    full_page = infinite_scroll(user_url)
+    user_page = urllib.request.urlopen(full_page)
+    get_tweets(user_page)
+
 if __name__ == "__main__":
-    # user_handle, user_bio, user_location = get_user_info(USER_PAGE)
-    get_tweets(USER_PAGE)
+    parser = argparse.ArgumentParser(description="This file scraps the twitter profile of the provided user and retrieves their tweets")
+    parser.add_argument('--user', required=True, help='The Twitter handle of the twitter user')
+    arg = parser.parse_args()
+    user = str(arg.user).replace("@", "")
+    main(user)
---
+++ src/sentiment_calculator.py
@@ -0,0 +1,12 @@
+'''
+This python file will calculate the Sentiment of whatever text is provided.  
+The following sentiments calculators are currently being utilized:
+    TextBlob
+    Vader Sentiment
+    AWS Comprehend (tentative)
+'''
+import scrapper
+from textblob import TextBlob as tb
+
+## TODO: Add vader and textblob sentiment after scrapping a twitter profile through selenium and BeautifulSoup
+
